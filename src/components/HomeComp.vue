3acb5dec6f2438d3bc1919971b1f2d80e83593d2
Connor Frendt <cjacksonfx@yahoo.com>
2023-02-21 13:56:20 -0800

    working on header for desktop/mobile
        edit 3d77cc6^

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        Home
+    <div id="home">
+        <div id="home-body">
+            Home
+        </div>
+
+        <div id="footer">
+            <div id="footer-links">
+                <div id="linkedin" class="socials">
+                    <a class="social-links" href="https://www.linkedin.com/in/connorfrendt" target="_blank">
+                        <font-awesome-icon icon="fa-brands fa-linkedin" />
+                    </a>
+                </div>
+                <div id="github" class="socials">
+                    <a class="social-links" href="https://www.github.com/connorfrendt" target="_blank">
+                        <font-awesome-icon icon="fa-brands fa-github" />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +28,42 @@ export default {
 </script>
 
 <style>
+#home {
+    height: 100%;
+    background-image: linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1));
+}
 
+#home-body {
+    height: 100%;
+}
+
+#footer-links {
+    /* position: absolute; */
+    /* text-align: center; */
+    /* left: calc(50% - 50px); */
+    /* bottom: 0; */
+    /* background-color: red; */
+    display: flex;
+    justify-content: center;
+}
+
+.socials {
+    padding: 10px;
+    font-size: 30px;
+}
+
+#linkedin {
+    /* border-right: 1px solid gray; */
+}
+
+.social-links {
+    color: black;
+}
+.social-links:visited {
+    text-decoration: none;
+}
+
+.social-links:active {
+    text-decoration: none;
+}
 </style>
