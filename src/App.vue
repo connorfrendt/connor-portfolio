92f53d009c474986c8707dafb7162b03b0e40200
Connor Frendt <connor@bridgesense.com>
2023-02-21 13:56:20 -0800

    working on header for desktop/mobile

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,11 @@ export default {
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+
 #app {
-    
+    height: 100vh;
 }
 </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,35 +1,49 @@
 <template>
-    <div id="header">
-        <RouterLink to="/" class="header-btn">
-            HOME
-        </RouterLink>
-        <RouterLink to="/resume" class="header-btn">
-            RESUME
-        </RouterLink>
-        <RouterLink to="/projects" class="header-btn">
-            PROJECTS
-        </RouterLink>
-        <RouterLink to="/aboutme" class="header-btn">
-            ABOUT ME
-        </RouterLink>
+    <div>
+        <!--Header-->
+        <div id="header">
+            <RouterLink to="/" class="header-btn">HOME</RouterLink>
+            <RouterLink to="/resume" class="header-btn">RESUME</RouterLink>
+            <RouterLink to="/projects" class="header-btn">PROJECTS</RouterLink>
+            <RouterLink to="/aboutme" class="header-btn">ABOUT ME</RouterLink>
+        </div>
+    
+        <!--Mobile Header-->
+        <div>
+    
+        </div>
     </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
+/*HEADER BUTTONS*/
 #header {
     display: flex;
-    /* justify-content: space-around; */
+    background-color: #03034C;
 }
 
 .header-btn {
     padding: 10px;
     font-size: 20px;
     font-family: 'Poppins', sans-serif;
+    text-decoration: none;
+    color: lightgrey;
+    
+}
+.header-btn:link {
+    text-decoration: none;
+}
+.header-btn:visited {
+    text-decoration: none;
 }
 .header-btn:hover {
-    background-color: lightblue;
-    transition: 0.25s ease-in-out;
+    background-color: #10107B;
+    transition: 0.15s ease-in-out;
 }
+.header-btn:active {
+    text-decoration: none;
+}
+
 </style>
