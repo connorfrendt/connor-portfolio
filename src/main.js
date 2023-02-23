56b07a8c222c3ab58921d2f492c5c119468c5cf4
Connor Frendt <connor@bridgesense.com>
2023-02-23 12:41:26 -0800

    dropdown finally working

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import router from './router.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHatWizard, faRobot } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './assets/tailwind.css'
 
-library.add(faHatWizard, faRobot, faLinkedin, faGithub);
+library.add(faLinkedin, faGithub, faBars);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
