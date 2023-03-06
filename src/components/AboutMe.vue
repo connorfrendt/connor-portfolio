65c5948ce0e94f5dc19f60889a382a8802d19956
Connor Frendt <cjacksonfx@yahoo.com>
2023-03-06 13:20:54 -0800

    wip parallax and rwb
    exec git commit --amend --author="Connor Frendt <cjacksonfx@yahoo.com>"

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        About Me
+        Full Stack JavaScript
+        <br />
+        <div class="line-through">Duveloper</div>
+        <div class="line-through">Developr</div>
+        <div class="line-through">Develupr</div>
+        <div>I'm good at making stuff</div>
     </div>
 </template>
 
--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,12 +1,39 @@
 <template>
     <div id="home">
         <div id="home-body">
-            <img />
-            <p id="quote">
-                "Sometimes life is like a dark tunnel.  You can't always see the light at the end of the tunnel, but if you just keep moving, you will come to a better place."
-            </p>
+
+            
+
+            <div id="pic-title">
+                <div>
+                    <img src="../assets/img/pfp.jpg" class="picture" />
+                </div>
+                
+                <div id="title">
+                    <div>FULL STACK</div>
+                    <div>JAVASCRIPT DEVELOPER</div>
+                </div>
+            </div>
+            
+            <div id="parallax">
+                <div id="quote">
+                    "Sometimes life is like a dark tunnel.
+                    <!-- <br /> -->
+                    You can't always see the light
+                    <!-- <br /> -->
+                    at the end of the tunnel,
+                    <!-- <br /> -->
+                    but if you just keep moving,
+                    <!-- <br /> -->
+                    you will come to a better place."
+                </div>
+            </div>
+
         </div>
 
+        
+
+        <!--Footer-->
         <div id="footer">
             <div id="footer-links">
                 <div id="linkedin" class="socials">
@@ -28,37 +55,106 @@
 export default {
     data() {
         return {
-            technologies: [
-                {
-                    id: 1,
-
-                }
-            ]
+        
         }
     }
 }
 </script>
 
 <style>
-#home {
-    height: 100%;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap');
+
+#parallax {
+    background-image: url('../assets/img/mt_hood_gorge.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+    min-height: 500px;
+    display: flex;
+    justify-content: center;
 }
 
 #home-body {
+    /* background-image: url('../assets/img/mt_hood_gorge.jpg');
     height: 100%;
-    padding: 20px;
-    color: white;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; */
 }
 
-#quote {
+#title {
+    color: white;
     text-align: center;
+    font-size: 40px;
+    font-family: 'Poppins', sans-serif;
     padding: 10px;
 }
 
-#technologies {
-    display: flex;
+@media screen and (0px <= width <= 640px) {
+    #pic-title {
+        padding: 20px;
+        height: 100%;
+    }
 
+    .picture {
+        border-radius: 10px;
+        height: 300px;
+        margin: 0 auto;
+    }
+
+    #quote {
+        padding: 0px;
+        margin-top: 10px;
+        background-color: rgba(120, 113, 108, 0.5);
+        color: white;
+        border-radius: 10px 10px 10px 10px;
+        text-align: center;
+    }
 }
+
+@media screen and (641px <= width <= 5000px) {
+    #title {
+        background-color: rgba(120, 113, 108, 0.5);
+        border-radius: 0px 10px 10px 0px;
+        height: 300px;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    #pic-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        height: 100%;
+    }
+
+    .picture {
+        border-radius: 10px 0px 0px 10px; /* tl tr br bl (clockwise) */
+        display: block;
+        margin: 0 auto;
+        height: 300px;
+    }
+
+    #quote {
+        background-color: rgba(120, 113, 108, 0.5);
+        border-radius: 10px;
+        height: 300px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        margin: 100px;
+        color: white;
+        font-size: 25px;
+    }
+}
+
+
 
 #footer-links {
     display: flex;
@@ -67,7 +163,6 @@ export default {
 }
 
 .socials {
-    /* padding: 10px; */
     font-size: 30px;
 }
 
