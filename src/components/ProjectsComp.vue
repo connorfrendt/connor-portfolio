7e7b42a0209a4416572e71b1950cca0edd6a439c
Connor Frendt <cjacksonfx@yahoo.com>
2023-06-06 18:51:41 -0700

    projects show and are working on the projects page
    exec git commit --amend --author="Connor Frendt <cjacksonfx@yahoo.com>"

--- src/components/ProjectsComp.vue
+++ src/components/ProjectsComp.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
-        <div id="proj-title">
-            Projects
-        </div>
-        <div id="sea-esta">
-            <a id="sea-esta-link" href="https://seaestacabo.com/" target="_blank">
-                <div class="sea-esta-title-div">
-                    <p id="sea-esta-title">Sea Esta Cabo Website</p>
+        <div class="proj-div">
+            <a class="website-link" href="https://seaestacabo.com/" target="_blank">
+                <div id="sea-esta-title-div">
+                    <p class="proj-title">Sea Esta Cabo Website</p>
                 </div>
             </a>
         </div>
+
+        <div class="proj-div">
+            <a class="website-link" href="https://lagringacabo.com/" target="_blank">
+                <div id="la-gringa-title-div">
+                    <p class="proj-title">La Gringa Cabo Website</p>
+                </div>
+            </a>
+        </div>
+
+        
     </div>
 </template>
 
@@ -20,35 +27,36 @@ export default {
 </script>
 
 <style scoped>
-#proj-title {
-    color: white;
-    text-align: center;
-    text-decoration: underline;
-    font-size: 35px;
+.proj-div {
     margin: 20px;
 }
 
-#sea-esta {
-    margin: 20px;
-}
-
-#sea-esta-link {
+.website-link {
     display: inline-block;
 }
-.sea-esta-title-div {
+#sea-esta-title-div {
     background-image: url("../assets/img/slide_sailboat_on_rocks.jpg");
     height: 250px;
     width: 400px;
     background-size: cover;
     position: relative;
-    margin: 40px 0 0 0;
     border-radius: 12px;
 }
 
-#sea-esta-title {
+#la-gringa-title-div {
+    background-image: url("../assets/img/lagringa-catamaran.jpg");
+    height: 250px;
+    width: 400px;
+    background-size: cover;
+    position: relative;
+    border-radius: 12px;
+}
+
+.proj-title {
     padding: 10px;
-    color: rgb(0, 0, 0);
-    background-color: orange;
+    color: rgb(255, 255, 255);
+    font-size: 22px;
+    background-color: rgba(0, 0, 0, .6);
     text-align: justify;
     position: absolute;
     bottom: 0;
