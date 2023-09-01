ac247b3ba1a3172bfd76dbdb9ebe2796aafc823a
Connor Frendt <cjacksonfx@yahoo.com>
2023-09-01 16:08:05 -0700

    wip about me page

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -15,7 +15,39 @@
             </div>
         </div>
 
-        <div>
+        <div class="text-white">
+            <div>
+                But why?...How?
+            </div>
+            <div>
+                These two questions are how I understand something.  Whenever there's a new concept, I'm always asking "why does it work?", and "how does that work?". Sure, 2 + 2 = 4, but if I don't have an intuitive understanding of what "2" is, it'll be hard to understand why those add up, and even harder to understand why d/dx [e^x] = e ^x, or why e^(iπ) + 1 = 0 (how can something with an imaginary number be equal to a real integer?  It's true though!).  Once I figure out how something works, I can use that knowledge over a broader scope and apply it to many different things, much like patterns...
+            </div>
+
+            <div>
+                Patterns, patterns, patterns...
+            </div>
+            <div>
+                Another process on how I learn is with patterns.  I could jump into the deep end of a programming language that I have zero knowledge on, figure out how it works, break it, fix it, and then add on to it - all because every programming language and application resorts to some sort of pattern.  For example, I once jumped into a Javascript project (back when I had no idea on what Javascript even was), poked around the code and figured out that it was comprised of three different parts - all behaving the same way.  I was able to see a couple working pieces, and a broken piece, and fix what was broken because I understood the pattern and how it worked - all in a language I didn't know!
+            </div>
+
+            <div>
+                I'm inquisitive by nature, flexible, adaptable, hard working, and if it's related to code (or math!), I'm very happy doing it.
+            </div>
+
+            <div class="text-center">
+                <div>Trivia!</div>
+                <div class="grid grid-cols-2">
+                    <div>Life long martial artist</div>
+                    <div>Love doing gymnastics</div>
+                    <div>Have been to 9 other countries (and counting!)</div>
+                    <div>Partner dancing since college</div>
+                    <div>Learning two other spoken languages at the same time</div>
+                    <div>Life long musician</div>
+                    <div>Big video and board game nerd</div>
+                    <div>Deep thinker</div>
+                    <div>A lover of life!</div>
+                </div>
+            </div>
 
         </div>
     </div>
