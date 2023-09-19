3d94142b4af82d9de61e673e78f35b756e7798e2
Connor Frendt <cjacksonfx@yahoo.com>
2023-09-19 15:06:39 -0700

    when switching to new page it starts at top - when using back button it goes to wherever you left off

--- src/router.js
+++ src/router.js
@@ -4,8 +4,12 @@ import ResumeComp from './components/ResumeComp.vue';
 import ProjectsComp from './components/ProjectsComp.vue';
 import ContactComp from './components/ContactComp.vue';
 import AboutMe from './components/AboutMe.vue';
+import Vue from 'vue';
+
+Vue.use(VueRouter);
 
 export default new VueRouter ({
+    mode: 'history',
     routes: [
         { path: '/', component: HomeComp },
         { path: '/resume', component: ResumeComp },
@@ -13,5 +17,15 @@ export default new VueRouter ({
         { path: '/contactme', component: ContactComp },
         { path: '/aboutme', component: AboutMe },
         { path: '*', redirect: '/' },
-    ]
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        if(savedPosition) {
+            // If a saved position is available, use it
+            return savedPosition;
+        }
+        else {
+            // Scroll to the top of the new page when navigating
+            return { x: 0, y: 0 };
+        }
+    }
 })
